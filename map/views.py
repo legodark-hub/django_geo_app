03088ae262189452f502f465e76b985b00e2d38a
aidarminyazev <legodark@mail.ru>
2025-10-22 22:37:49 +0500

    add frontend

--- map/views.py
+++ map/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def map_view(request):
-    return HttpResponse("здесь будет карта")
+    return render(request, 'index.html')
